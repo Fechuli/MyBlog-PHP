e967ab6c9f03393f80e5d4b58378467cc455a4a3
Federico Fiaschi <99290773+Fechuli@users.noreply.github.com>
2023-11-24 10:03:33 +0100

    Delete admin directory

--- admin/templates/admin.php
+++
@@ -1,7 +0,0 @@
-<?php require __DIR__ . '/../../templates/header.php'; ?>
-<h2>Amministrazione</h2>
-<ul>
-  <li><a href="?action=list-posts">Lista dei Post</a></li>
-  <li><a href="?action=new-post">Nuovo Post</a></li>
-</ul>
-<?php require __DIR__ . '/../../templates/footer.php'; ?>
